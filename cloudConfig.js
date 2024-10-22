181bf36b90c93a2463c2c9da76e4721d1ad47721
pawang2005 <kumar.pawan2005g@gmail.com>
2024-10-22 09:40:51 +0530

    Final Changes Done

--- cloudConfig.js
+++ cloudConfig.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'litto',
+        folder: 'litter',
         allowedFormats: ["png", "jpg", "jpeg"],
     },
 });
